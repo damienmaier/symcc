80ee4f1256ba115149457b8b6977e71afb0ad1b7
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2023-11-22 16:04:19 +0000

    log symbolic areas

--- runtime/qsym_backend/Runtime.cpp
+++ runtime/qsym_backend/Runtime.cpp
@@ -19,6 +19,7 @@
 
 #include "Runtime.h"
 #include "GarbageCollection.h"
+#include "ShadowTrace.h"
 
 // C++
 #if __has_include(<filesystem>)
@@ -409,6 +410,7 @@ void _sym_notify_ret(uintptr_t site_id) {
 }
 
 void _sym_notify_basic_block(uintptr_t site_id) {
+  dumpSymbolicState();
   g_call_stack_manager.visitBasicBlock(site_id);
 }
 
